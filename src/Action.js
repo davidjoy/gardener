097b1798da130a0e25876848cb649ad2cdc71e70
davidjoy <davjoy@gmail.com>
2019-09-12 23:38:05 -0400

    Animate in, my pretties!

--- src/Action.js
+++ src/Action.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Action(props) {
-    return <button className={`action ${props.rotation}`}>
+    return <button style={{ animationDelay: props.delay }} className={`action ${props.rotation}`}>
         {/* <FontAwesomeIcon icon={props.icon} /> */}
         {props.label}
     </button>
--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import Viewport from './Viewport';
 import Action from './Action';
 
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
     return (
         <main>
             <div className="wrapper">
                 <Viewport />
-
-                <Action rotation="deg255" label="Walk" />
-                <Action rotation="deg240" label="Look" />
-                <Action rotation="deg225" label="Use" />
-                <Action rotation="deg210" />
-                <Action rotation="deg195" />
-                <Action rotation="deg180" />
+                <Action rotation="deg255" delay="0s" label="Walk" />
+                <Action rotation="deg240" delay="0.2s" label="Look" />
+                <Action rotation="deg225" delay="0.4s" label="Use" />
+                <Action rotation="deg210" delay="0.6s" label="Attack" />
+                <Action rotation="deg195" delay="0.8s" label="Sheet" />
+                <Action rotation="deg45" delay="1s" label="Exit" />
             </div>
         </main>
     );
